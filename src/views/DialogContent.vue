4834e0aed18577a1666bb2a719c33e1a69c9d022
Rafael Neves <rafael@meuportfol.io>
2020-09-16 14:55:29 -0300

    content to dialog

--- src/views/DialogContent.vue
+++ src/views/DialogContent.vue
@@ -1,6 +1,8 @@
 <template>
   <v-container>
-    Dialog Content
+    <div class="display-3 font-weight-bold">Dialog Content</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, vitae nemo! Odit ad voluptas, aliquid vel,
+      veniam molestiae quos beatae corporis laborum quam, doloribus ullam optio adipisci? Fugiat, commodi dolore.</div>
   </v-container>
 </template>
 
